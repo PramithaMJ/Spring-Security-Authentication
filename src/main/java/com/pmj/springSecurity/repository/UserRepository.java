f15491989c685499c619ae08bd4c1deb28d2daf4
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-03-29 18:11:51 +0530

    user repo

---
+++ src/main/java/com/pmj/springSecurity/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pmj.springSecurity.repository;
+
+import com.pmj.springSecurity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
